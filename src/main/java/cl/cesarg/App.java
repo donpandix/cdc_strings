86994db7937034a309302568dd6c52773f82beef
Cesar Gonzalez <cesar@cesarg.cl>
2019-01-09 15:58:59 -0300

    Initial commit

---
+++ src/main/java/cl/cesarg/App.java
@@ -0,0 +1,17 @@
+package cl.cesarg;
+
+/**
+ * Implementación de los casos de prueba del libro Cracking The Code
+ * particularmente del capítulo 1
+ *
+ * @author  Cesar Gonzalez       @donpandix
+ * @version v.20190109
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("\nCasos de prueba");
+    }
+}
